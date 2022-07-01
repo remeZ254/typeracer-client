e549dfec9a2571b612825d914daae057391fb4b8
remeZ <zemer.hi@gmail.com>
2022-07-01 22:57:17 +0300

    form improvements

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -2,16 +2,17 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/auth/service/auth.service';
 
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  auth: string = '';
+  showError: boolean = false;
+  shakeInput: boolean = false;
   readonly creatorName: string = 'Zemer Hiyret';
   readonly creatorNumber: string = '530-2525';
-  auth: string = '';
 
   constructor(private authService: AuthService, private router: Router) {}
 
@@ -19,6 +20,15 @@ export class LoginComponent {
     if (this.auth) {
       this.authService.logIn(this.auth);
       this.router.navigate(['/']);
+    } else {
+      this.showError = true;
+      this.shakeInput = true;
     }
+  }
+
+  onInput(event: Event) {
+    this.auth = (event.target as HTMLTextAreaElement).value;
+    this.showError = !this.auth;
+    this.shakeInput = false;
   }
 }
