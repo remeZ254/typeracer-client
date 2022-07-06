76e1a09c0e4547d9f6827498a8a6a34d051aeeb5
remeZ <zemer.hi@gmail.com>
2022-07-06 20:16:10 +0300

    optimize quote format

--- src/app/features/room/container/room.component.ts
+++ src/app/features/room/container/room.component.ts
@@ -6,18 +6,19 @@ import { Room } from '@app/shared/models/room/room.model';
 import { select, Store } from '@ngrx/store';
 import { first, Observable, of } from 'rxjs';
 
-interface InvalidDisplay {
-  invalidInProgress: string;
-  inProgress: string;
-  invalid: string;
-  rest: string;
-}
-
-type ValidDisplay = Omit<InvalidDisplay, `invalid${string}`>;
-
-interface CompletedDisplay {
-  completed: string;
-  completedInProgress: string;
+interface QuoteDisplay {
+  completed: {
+    inProgress: string;
+    rest: string;
+  };
+  valid: {
+    inProgress: string;
+    rest: string;
+  };
+  invalid: {
+    inProgress: string;
+    rest: string;
+  };
 }
 
 @Component({
@@ -27,13 +28,15 @@ interface CompletedDisplay {
 })
 export class RoomComponent {
   input: string = '';
-  valid: boolean = true;
-  completedDisplay: CompletedDisplay = { completed: '', completedInProgress: '' };
-  validDisplay: ValidDisplay = { inProgress: '', rest: '' };
-  invalidDisplay: InvalidDisplay = { invalidInProgress: '', inProgress: '', invalid: '', rest: '' };
+  isInvalid: boolean = false;
+  quoteDisplay: QuoteDisplay = {
+    completed: { inProgress: '', rest: '' },
+    valid: { inProgress: '', rest: '' },
+    invalid: { inProgress: '', rest: '' },
+  };
   readonly room$: Observable<Room>;
-  private completedWords: string[] = [];
   private completedChars: string = '';
+  private completedWords: string[] = [];
   private uncompletedWords: string[] = [];
   private wordIndex: number = 0;
 
@@ -42,45 +45,58 @@ export class RoomComponent {
     this.room$ = of(roomMock);
     this.room$.pipe(first()).subscribe(({ text: { quote } }) => {
       this.uncompletedWords = quote.split(' ');
-      this.validDisplay = {
-        inProgress: this.uncompletedWords[0],
-        rest: this.uncompletedWords.slice(1).join(' '),
-      };
+      this.formatDisplay();
     });
   }
 
   onInput() {
     const correctInput = `${this.uncompletedWords[0]} `;
+
     if (this.input === correctInput) {
       this.wordIndex++;
       this.input = '';
       this.uncompletedWords.shift();
       this.completedWords.push(correctInput);
-      this.completedDisplay.completed = this.completedWords.join(' ');
     }
 
-    if (!correctInput.startsWith(this.input)) {
-      this.valid = false;
-      const allWords = this.uncompletedWords.join(' ');
-      const firstLength = this.uncompletedWords[0].length;
-      const inputLength = this.input.length;
-      const completedCharsLength = this.completedChars.length;
-
-      this.invalidDisplay = {
-        invalidInProgress: allWords.slice(completedCharsLength, Math.min(firstLength, inputLength)),
-        inProgress: allWords.slice(inputLength, firstLength),
-        invalid: allWords.slice(firstLength, inputLength),
-        rest: allWords.slice(Math.max(firstLength, inputLength)),
-      };
-    } else if (correctInput.startsWith(this.input)) {
-      this.valid = true;
+    if (correctInput.startsWith(this.input)) {
       this.completedChars = this.input;
-      this.completedDisplay.completedInProgress = this.completedChars;
     }
 
-    this.validDisplay = {
-      inProgress: this.uncompletedWords[0].slice(this.completedChars.length),
-      rest: this.uncompletedWords.slice(1).join(' '),
+    this.formatDisplay();
+  }
+
+  private formatDisplay() {
+    this.isInvalid = !!(this.input && this.input !== this.completedChars);
+
+    this.quoteDisplay = {
+      completed: {
+        inProgress: this.completedChars,
+        rest: this.completedWords.join(' '),
+      },
+      valid: {
+        inProgress: this.isInvalid
+          ? this.uncompletedWords
+              .join(' ')
+              .slice(this.input.length, this.uncompletedWords[0].length)
+          : this.uncompletedWords[0].slice(this.completedChars.length),
+        rest: this.isInvalid
+          ? this.uncompletedWords
+              .join(' ')
+              .slice(Math.max(this.uncompletedWords[0].length, this.input.length))
+          : ` ${this.uncompletedWords.slice(1).join(' ')}`,
+      },
+      invalid: {
+        inProgress: this.uncompletedWords
+          .join(' ')
+          .slice(
+            this.completedChars.length,
+            Math.min(this.uncompletedWords[0].length, this.input.length)
+          ),
+        rest: this.uncompletedWords
+          .join(' ')
+          .slice(this.uncompletedWords[0].length, this.input.length),
+      },
     };
   }
 }
