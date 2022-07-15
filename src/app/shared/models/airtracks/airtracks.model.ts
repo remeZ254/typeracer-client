160d5e3d3a01b319a2f6250f0d8f89b8504de862
remeZ <zemer.hi@gmail.com>
2022-07-15 16:34:21 +0300

    add barak raam and sufa

--- src/app/shared/models/airtracks/airtracks.model.ts
+++ src/app/shared/models/airtracks/airtracks.model.ts
@@ -1,6 +1,9 @@
 export enum AirTracks {
   adir = 'adir',
+  barak = 'barak',
   baz = 'baz',
+  raam = 'raam',
+  sufa = 'sufa',
 }
 
 export const getTrackIcon = (airTrack: AirTracks): string => `assets/svg/airtracks/${airTrack}.svg`;
