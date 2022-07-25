4933f256f8bf5afe2c3310407122799ca36077ce
remeZ <zemer.hi@gmail.com>
2022-07-25 20:09:45 +0300

    FIX wrong ngrx action

--- src/app/core/theme/actions/theme.actions.ts
+++ src/app/core/theme/actions/theme.actions.ts
@@ -2,7 +2,7 @@ import { Themes } from '@app/shared/models/themes/themes.enum';
 import { createAction, props } from '@ngrx/store';
 
 enum ThemeActions {
-  CHANGE_THEME = '[Room] Connect',
+  CHANGE_THEME = '[Theme] Change',
 }
 
 export const changeTheme = createAction(ThemeActions.CHANGE_THEME, props<{ theme: Themes }>());
